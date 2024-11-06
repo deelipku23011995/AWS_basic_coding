2a48a0ba0fdd4eb386e609fd012fb26935bf9ba0
deelipku23011995 <deelipku23@gmail.com>
2024-11-06 19:12:14 +0530

    Delete Lambda Function layer and container /Lambda container Local and container deployment/example directory

--- Lambda Function layer and container /Lambda container Local and container deployment/example/lambda_function.py
+++
@@ -1,6 +0,0 @@
-import sys
-import numpy
-def handler(event, context):
-    print("event:-----",event)
-#     return 'Hello from AWS Lambda using Python' + sys.version + '!  array is :--'+ str(numpy.random.rand(2))
-    return event
